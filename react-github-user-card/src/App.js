af81d8a5b0c93b06641f0974a7d07918d3b55ef6
Chase Hooper <43388614+HoopCha@users.noreply.github.com>
2019-10-30 08:24:35 -0600

    finished some stretch, search

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -13,7 +13,8 @@ constructor() {
   super();
     this.state = {
       mycard: [],
-      followers: []
+      followers: [],
+      userInput: ""
   };
 }
 
@@ -34,15 +35,45 @@ componentDidMount(){
     .catch(err => console.log('error'));
   }
 
-  componentDidUpdate(){
-    
-  }
+  handleChanges = e => {
+    this.setState({
+      userInput: e.target.value
+    });
+  };
+
+
+  fetchPeople = () => {
+    axios
+      .get(`https://api.github.com/users/${this.state.userInput}`)
+      .then(res => {
+        this.setState({
+          mycard: res.data
+        });
+      })
+      .catch(err => console.log(err));
+    axios
+    .get(`https://api.github.com/users/${this.state.userInput}/followers`)
+    .then((response) => {
+      console.log("ahhhhh", response)
+      this.setState({
+        followers: response.data
+      });
+    })
+    .catch(err => console.log('error'));
+  };
+
 
 render(){
   return (
   <div>
     <div>
       <h1>GitHub Cards</h1>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleChanges}
+        />
+        <button onClick={this.fetchPeople}>Search Username</button>
       <div>
         <MyCard
         key = {this.state.mycard.created_at}
